204510e48eb0c03556893a664e06eb064bc8eadf
urmangit <wojteku229@icloud.com>
2025-10-17 19:12:59 +0200

    feat: initial commit

---
+++ builtins/__init__.py
@@ -0,0 +1,14 @@
+from pygame.math import Vector2 as v2
+from pygame.typing import ColorLike, RectLike
+from pygame.surface import Surface
+from pygame.rect import Rect
+from pygame import init
+from sys import exit
+
+__all__ = ['v2',
+           'ColorLike',
+           'RectLike',
+           'Surface',
+           'Rect',
+           'init',
+           'exit']
