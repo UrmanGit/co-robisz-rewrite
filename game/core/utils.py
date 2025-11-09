11446d7037180ef55ab0caea242ae381d2fb46f8
urmangit <wojteku229@icloud.com>
2025-11-09 13:46:55 +0100

    feat[utils.py]: add load_animations() func that loads .gif files from data/animations
    add data/animations folder where .gif files are stored

--- game/core/utils.py
+++ game/core/utils.py
@@ -4,14 +4,20 @@ pg.init()
 import pathlib
 from . import config as cfg
 
-def load_image(path: pathlib.Path) -> pg.Surface:
-    return pg.image.load(path)
-
 def load_images(path: pathlib.Path) -> dict[str, pg.Surface]:
     images: dict[str, pg.Surface] = {}
     for path in (cfg.DATA / 'images').rglob("*.png"):
-        image = load_image(path)
+        image = pg.image.load(path)
         image = image.convert_alpha() if image.get_alpha() else image.convert()
         relative_path = path.relative_to(cfg.DATA / 'images')
         images[str(relative_path)] = image
     return images
+
+def load_animations(path: pathlib.Path) -> dict[str, list[tuple[pg.Surface, float]]]:
+    animations: dict[str, list[tuple[pg.Surface, float]]] = {}
+    for path in (cfg.DATA / 'animations').rglob("*.gif"):
+        animation = pg.image.load_animation(path)
+        for pair in animation:
+            pair[0].convert_alpha() if pair[0].get_alpha() else pair[0].convert()
+        animations[str(path.relative_to(cfg.DATA / 'animations'))] = animation
+    return animations
--- game/tests/image_load_test.py
+++ game/tests/image_load_test.py
@@ -3,10 +3,10 @@ import pygame as pg
 from pprint import pprint
 pg.init()
 
-display = pg.display.set_mode((800, 600))
+# display = pg.display.set_mode((800, 600))
 
 from game.core.utils import load_images
 from game.core.config import DATA
 
 example = load_images(DATA)
-pprint(example)
+#pprint(example)
