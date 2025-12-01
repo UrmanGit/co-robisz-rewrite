fb92fec566b4d7e1c9ef894058bb6d3018027ee3
urmangit <wojteku229@icloud.com>
2025-12-01 01:26:25 +0100

    fix[utils.py]: rename "path" arg in load_ functions to "basepath"
    add load_image function that load one image

--- game/core/assets.py
+++ game/core/assets.py
@@ -1,6 +1,7 @@
-from game.core.utils import load_animations, load_images
+from game.core.utils import load_animations, load_images, load_image
 from game.core.config import DATA
 from pygame import Surface
 
 images: dict[str, Surface] = load_images(DATA)
+image = (lambda path: load_image(path))
 animations: dict[str, list[tuple[Surface, float]]] = load_animations(DATA)
--- game/core/utils.py
+++ game/core/utils.py
@@ -1,22 +1,26 @@
 import pygame as pg
 import pathlib
-import game.core.config as cfg
 
-def load_images(path: pathlib.Path) -> dict[str, pg.Surface]:
+def load_images(basepath: pathlib.Path) -> dict[str, pg.Surface]:
     images: dict[str, pg.Surface] = {}
-    for path in (path / 'images').rglob("*.png"):
+    for path in (basepath / 'images').rglob("*.png"):
         image = pg.image.load(path)
         image = image.convert_alpha() if image.get_alpha() else image.convert()
-        relative_path = path.relative_to(path / 'images')
+        relative_path = path.relative_to(basepath / 'images')
         images[str(relative_path)] = image
     return images
 
-def load_animations(path: pathlib.Path) -> dict[str, list[tuple[pg.Surface, float]]]:
+def load_image(basepath: pathlib.Path) -> pg.Surface:
+    image = pg.image.load(basepath)
+    image = image.convert_alpha() if image.get_alpha() else image.convert()
+    return image
+
+def load_animations(basepath: pathlib.Path) -> dict[str, list[tuple[pg.Surface, float]]]:
     animations: dict[str, list[tuple[pg.Surface, float]]] = {}
-    for path in (path / 'animations').rglob("*.gif"):
+    for path in (basepath / 'animations').rglob("*.gif"):
         animation = pg.image.load_animation(path)
         for pair in animation:
             pair[0].convert_alpha() if pair[0].get_alpha() else pair[0].convert()
-        relative_path = path.relative_to(path / 'animations')
+        relative_path = path.relative_to(basepath / 'animations')
         animations[str(relative_path)] = animation
     return animations
