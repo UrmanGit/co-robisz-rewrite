aaec8a58965437f79bc44db1ae990f957d300347
urmangit <wojteku229@icloud.com>
2025-12-11 21:25:43 +0100

    feat(room.py): "Finish" the `Room` class and draw a first room
    finally some visible progress

--- game/map/tilemaps.py
+++ game/map/tilemaps.py
@@ -1,6 +1,16 @@
 import pygame as pg
+import game.core.assets as assets
 
 magazine_tilemap: list[list[int]] = [[1, 1, 2, 1],
                                      [0, 0, 0, 0],
                                      [3, 3, 3, 3]]
-magazine_tiles: dict[int, pg.Surface] | dict[int, None] = {1: None, 2: None, 3: None}
+magazine_tiles: dict[int, pg.Surface] = {}
+
+
+def load_tiles() -> None:
+    global magazine_tiles
+    magazine_tiles = {
+        0: assets.images["rooms/magazine/magazine_floor.png"],
+        1: assets.images["rooms/magazine/magazine_wall.png"],
+        2: assets.images["rooms/magazine/closed_magazine_door.png"],
+        3: assets.images["outer_walls/hor.png"]}
