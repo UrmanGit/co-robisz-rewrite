0b8a9cf71d0388d0da95c43b05e7f936545d50af
urmangit <wojteku229@icloud.com>
2025-12-11 21:24:16 +0100

    feat(utils.py): upgraded the functions so they scale the image by provided scale factor

--- game/core/utils.py
+++ game/core/utils.py
@@ -4,29 +4,49 @@ import pathlib
 from pygame import Color
 
 
-def load_images(path: pathlib.Path) -> dict[str, pg.Surface]:
+def load_images(path: pathlib.Path, scale_factor: int) -> dict[str, pg.Surface]:
     """Recursively loads all .png files from a given folder"""
     images: dict[str, pg.Surface] = {}
     for file_path in path.rglob("*.png"):
         image = pg.image.load(file_path)
         image = image.convert_alpha() if image.get_alpha() else image.convert()
+        image = pg.transform.scale_by(image, scale_factor)
         relative_path = file_path.relative_to(path)
-        images[str(relative_path)] = image
+        images[str(relative_path).replace("\\", "/")] = image
     return images
 
-def load_image(path: pathlib.Path) -> pg.Surface:
+
+def load_image(path: pathlib.Path, scale_factor: int) -> pg.Surface:
+    """Load a single image"""
     image = pg.image.load(path)
     image = image.convert_alpha() if image.get_alpha() else image.convert()
+    image = pg.transform.scale_by(image, scale_factor)
     return image
 
-def load_animations(path: pathlib.Path) -> dict[str, list[tuple[pg.Surface, float]]]:
+
+def load_animations(path: pathlib.Path, scale_factor: int) -> dict[str, list[tuple[pg.Surface, float]]]:
+    """Recursively loads all .gif files from a given folder"""
     animations: dict[str, list[tuple[pg.Surface, float]]] = {}
+
     for file_path in path.rglob("*.gif"):
-        animation = pg.image.load_animation(file_path)
-        for pair in animation:
-            pair[0].convert_alpha() if pair[0].get_alpha() else pair[0].convert()
+        # Loading the animation as normal
+        animation: list[tuple[pg.Surface, float]] = pg.image.load_animation(file_path)
+
+        for i, pair in enumerate(animation):
+            # 1. Rip the pair apart
+            frame, delay = pair
+
+            # 2. Convert the frame and resize it
+            frame.convert_alpha() if frame.get_alpha() else frame.convert()
+            frame = pg.transform.scale_by(frame, scale_factor)
+
+            # 3. Put everything together like nothing happened
+            animation[i] = (frame, delay)
+
+            # 4. Surgery completed successfully
+
         relative_path = file_path.relative_to(path)
-        animations[str(relative_path)] = animation
+        animations[str(relative_path).replace("\\", "/")] = animation
     return animations
 
 
