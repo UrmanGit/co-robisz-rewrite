be95403883db0252e3e7c5a8bc398fbc1b0fc592
urmangit <wojteku229@icloud.com>
2025-12-11 21:25:43 +0100

    feat[room.py]: "Finish" the `Room` class and draw a first room
    finally some visible progress

--- game/core/game.py
+++ game/core/game.py
@@ -1,4 +1,5 @@
 import pygame as pg
+
 pg.init()
 
 import sys
@@ -9,7 +10,13 @@ import game.core.utils as utils
 from game.core.window import WindowManager
 import game.core.assets as assets
 
+import game.map.room
+import game.map.tilemaps
+
+
 class Game:
+    """A game"""
+
     def __init__(self) -> None:
         """Here you define the variables"""
         # Create a screen
@@ -19,16 +26,26 @@ class Game:
         assets.dict_images()
         assets.dict_animations()
 
+        # Other delayed loadings
+        game.map.tilemaps.load_tiles()
+
         # Pygame stuff
         self.keys: ScancodeWrapper = pg.key.get_pressed()
         self.just_keys: ScancodeWrapper = pg.key.get_just_pressed()
-        self.mouse: tuple[bool, bool, bool] = pg.mouse.get_pressed()
+        self.mouse: tuple[bool, bool, bool, bool, bool] = pg.mouse.get_pressed(5)
         self.just_mouse: tuple[bool, bool, bool, bool, bool] = pg.mouse.get_just_pressed()
         self.mouse_pos: tuple[int, int] = pg.mouse.get_pos()
         self.clock: pg.Clock = pg.time.Clock()
+
+        # Window title and icon
         pg.display.set_caption(config.TITLE)
-        import game.core.assets as ast
-        pg.display.set_icon(ast.image(config.DATA / "icon.png"))
+        pg.display.set_icon(assets.image(config.DATA / "icon.png"))
+
+        # Main stuff
+        self.magazine = game.map.room.Room((config.BASE_RESOLUTION[0] // 2,
+                                            config.BASE_RESOLUTION[1] // 2),
+                                           tilemap=game.map.tilemaps.magazine_tilemap,
+                                           tiles=game.map.tilemaps.magazine_tiles)
 
     def events(self) -> None:
         for event in pg.event.get():
@@ -49,6 +66,7 @@ class Game:
 
     def draw(self) -> None:
         self.wm.screen.fill(utils.hex_col("#0C0C12"))
+        self.magazine.draw(screen = self.wm.screen)
         pg.display.flip()
 
     def run(self) -> None:
--- game/map/room.py
+++ game/map/room.py
@@ -1,4 +1,20 @@
-from game.core.config import *
+import pygame as pg
+
+import game.core.config as config
+
 
 class Room:
-    def __init__(self, offset: tuple[int, int], tilemap: list[list[int]]) -> None: ...
+    def __init__(self, offset: tuple[int, int], tilemap: list[list[int]], tiles: dict[int, pg.Surface]) -> None:
+        self.pos: pg.Vector2 = pg.Vector2(offset)
+        self.tilemap: list[list[int]] = tilemap
+        self.tiles: dict[int, pg.Surface] = tiles
+        self.hitbox: pg.Rect = pg.Rect(self.pos,
+                                       (config.TILE_SIZE * len(tilemap[0]),
+                                        config.TILE_SIZE * len(tilemap)))
+        
+    def draw(self, screen: pg.Surface) -> None:
+        for i, row in enumerate(self.tilemap):
+            for j, tile in enumerate(row):
+                tile_left = self.pos.x + config.TILE_SIZE * j
+                tile_top = self.pos.y + config.TILE_SIZE * i
+                screen.blit(self.tiles[tile], (tile_left, tile_top))
