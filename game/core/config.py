96df15e151bc05af2b1e832cb76f4306d16f739b
urmangit <wojteku229@icloud.com>
2025-12-11 21:22:17 +0100

    style(images): renamed `outerwalls` to `outer_walls`

--- game/core/config.py
+++ game/core/config.py
@@ -7,3 +7,5 @@ BASE_RESOLUTION: tuple[int, int] = (1920, 1080)
 TITLE: str = "Co robisz? - rewrite (DEV)"
 FPS: int = 60
 DATA: pathlib.Path = pathlib.Path(__file__).parent.parent.parent / "data"
+SCALE_FACTOR: int = 3
+TILE_SIZE: int = 24 * SCALE_FACTOR
