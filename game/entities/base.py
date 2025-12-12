7f8c2334647b62d239cd50b7e9c6ca189f17cf91
urmangit <wojteku229@icloud.com>
2025-12-12 17:29:03 +0100

    feat: add entities package, base.py with unfinished Movable class

---
+++ game/entities/base.py
@@ -0,0 +1,11 @@
+from pygame.math import Vector2
+
+
+class Movable:
+    def __init__(self, pos: tuple[int, int] | Vector2, speed: int) -> None:
+        self.pos: Vector2 = Vector2(pos)
+        self.speed: int = speed
+
+    def move(self):
+        ... # TODO: ADD MOVE FUNCTION WITH HITBOX CHECKING AND SQUARE ROOT DIAGONAL MOVING.
+
