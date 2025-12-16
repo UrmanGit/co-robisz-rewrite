a4b99d99dfd913bc292e6335a672f3f4a3d836e7
urmangit <wojteku229@icloud.com>
2025-12-16 20:58:23 +0100

    fix: Make player slower and remove debug print

--- game/core/config.py
+++ game/core/config.py
@@ -5,8 +5,8 @@ import pathlib
 # Constants
 BASE_RESOLUTION: tuple[int, int] = (1920, 1080)
 TITLE: str = "Co robisz? - rewrite (DEV)"
-FPS: int = 60
+FPS: int = 180
 DATA: pathlib.Path = pathlib.Path(__file__).parent.parent.parent / "data"
 SCALE_FACTOR: int = 3
 TILE_SIZE: int = 24 * SCALE_FACTOR
-PLAYER_SPEED: int = 200
+PLAYER_SPEED: int = 150
