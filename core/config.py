9de951401117d53af7f0962c95ce1c73e7c25d89
UrmanGit <wojteku229@icloud.com>
2026-02-11 19:53:44 +0100

    style: rename `data/` to `assets/` because why not

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ import pathlib
 BASE_RESOLUTION: tuple[int, int] = (1920, 1080)
 TITLE: str = "Co robisz? - rewrite (DEV)"
 FPS: int = 180
-DATA: pathlib.Path = pathlib.Path(__file__).parent.parent / "data"
+DATA: pathlib.Path = pathlib.Path(__file__).parent.parent / "assets"
 SCALE_FACTOR: int = 3
 TILE_SIZE: int = 24 * SCALE_FACTOR
 PLAYER_SPEED: int = 150
